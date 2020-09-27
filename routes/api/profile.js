4c3c7bf34acaaa80f5fa40018e7cd86a5a5fa134
tebmik <lloyd.s.tom@gmail.com>
2020-09-27 10:49:06 +0100

    public get request made to retrieve all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -107,4 +107,21 @@ router.post(
     }
 );
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', [
+            'name',
+            'avatar',
+        ]);
+        res.send(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
