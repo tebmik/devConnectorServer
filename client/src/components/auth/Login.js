a412cc8b935d72e8f1528e8d04f105ec27877bd3
tebmik <lloyd.s.tom@gmail.com>
2020-09-28 13:17:48 +0100

    Login form with input handles

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+    const { email, password } = formData;
+
+    const handleChange = e =>
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log('Success');
+    };
+
     return (
         <>
-            <h1>Login</h1>
+            <section className='container'>
+                <h1 className='large text-primary'>Sign In</h1>
+                <p className='lead'>
+                    <i className='fas fa-user'></i> Sign Into Your Account
+                </p>
+                <form
+                    className='form'
+                    action='create-profile.html'
+                    onSubmit={handleSubmit}
+                >
+                    <div className='form-group'>
+                        <input
+                            type='email'
+                            placeholder='Email Address'
+                            name='email'
+                            value={email}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='password'
+                            placeholder='Password'
+                            name='password'
+                            minLength='6'
+                            value={password}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <input
+                        type='submit'
+                        className='btn btn-primary'
+                        value='Login'
+                    />
+                </form>
+                <p className='my-1'>
+                    Dont have an account? <Link to='/register'>Sign up</Link>
+                </p>
+            </section>
         </>
     );
 };
