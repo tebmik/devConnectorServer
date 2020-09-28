8c9f3c5739e3f584fd8ad73f6247ec57d4a6c134
tebmik <lloyd.s.tom@gmail.com>
2020-09-28 12:00:37 +0100

    Landing page created for the client-side

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+import './App.css';
+
+const App = () => (
+    <>
+        <Navbar />
+        <Landing />
+    </>
+);
+
+export default App;
