35e49f2fb07a1a1ad5d72e9551da8d6c7c8c8068
tebmik <lloyd.s.tom@gmail.com>
2020-09-28 12:25:15 +0100

    react-router-dom added for navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './App.css';
 
 const App = () => (
     <>
-        <Navbar />
-        <Landing />
+        <BrowserRouter>
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+
+            <section className='container'>
+                <Switch>
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/register' component={Register} />
+                </Switch>
+            </section>
+        </BrowserRouter>
     </>
 );
 
