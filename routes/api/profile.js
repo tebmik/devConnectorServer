7c8ec19be044d4077ce1c17dde6a90e380f2ba96
tebmik <lloyd.s.tom@gmail.com>
2020-10-14 16:55:45 +0100

    remove posts node api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,7 @@ const { check, validationResult } = require('express-validator');
 // models
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -152,6 +153,7 @@ router.get('/user/:user_id', async (req, res) => {
 router.delete('/', auth, async (req, res) => {
     try {
         //@todo - remove users posts
+        await Post.deleteMany({ user: req.user.id });
         //Remove Profile
         await Profile.findOneAndRemove({ user: req.user.id });
         //Remove user
